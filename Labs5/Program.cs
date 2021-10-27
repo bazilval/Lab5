f745381ef48cf02cb5d6b19c27ad2f8a429120b1
bazilval <bazilval@bk.ru>
2021-10-27 22:20:33 +0300

    Добавьте файлы проекта.

---
+++ Labs5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Сформировать одномерный массив из 7 элементов.
+ * Заполнить массив числами, вводимыми с клавиатуры, 
+ * определить среднее арифметическое элементов.*/
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[7];
+            float sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"Введите значение элемента {i+1}: ");
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            foreach (int e in arr)
+            {
+                sum += e;
+            }
+            Console.WriteLine($"Среднее арифметическое массива = {sum/arr.Length,3:f3}");
+            Console.ReadKey();
+        }
+    }
+}
